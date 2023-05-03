dd0d663c33a836ca302e2d516c69a7bc7d8bf597
Aleksandar Grbic <hi@aleksandar.xyz>
2023-05-03 13:01:34 +0200

    6sense

--- 6sense.js
+++ 6sense.js
@@ -1,24 +1,32 @@
 window.analytics.ready(function () {
-    if (window.localStorage && localStorage["_6senseCompanyDetails"] && !analytics.group().traits().website) {
-        try {
-            const details = JSON.parse(localStorage["_6senseCompanyDetails"]);
-
-            if (!details || !details.company || !details.company.domain || analytics.group().traits().website) {
-                return;
-            }
-
-            const reveal = details.company;
-            const company = {
-                website: reveal.domain,
-                source: '6sense',
-                name: reveal.name || undefined,
-                country: reveal.country || undefined,
-                industry: reveal.industry || undefined,
-                number_of_employees: reveal.employee_range || undefined,
-                annual_revenue: reveal.revenue_range || undefined,
-            };
-
-            analytics.group(null, company);
-        } catch (e) { }
+  function getStorage(lsKey) {
+    try {
+      return JSON.parse(localStorage.getItem(lsKey));
+    } catch (e) {
+      return null;
     }
+  }
+
+  const details = getStorage("_6senseCompanyDetails");
+
+  if (
+    !details ||
+    !details.company ||
+    !details.company.domain ||
+    analytics.group().traits().website
+  ) {
+    return;
+  }
+
+  const reveal = details.company;
+
+  analytics.group(null, {
+    website: reveal.domain,
+    source: "6sense",
+    name: reveal.name || undefined,
+    country: reveal.country || undefined,
+    industry: reveal.industry || undefined,
+    number_of_employees: reveal.employee_range || undefined,
+    annual_revenue: reveal.revenue_range || undefined,
+  });
 });
