996ee1b229feeaabcd63c96cc14d8b54c947b3cc
Aleksandar Grbic <hi@aleksandar.xyz>
2023-05-03 13:16:23 +0200

    clearbit

--- clearbit.js
+++ clearbit.js
@@ -1,29 +1,36 @@
 function clearbitCallback(response) {
-    sessionStorage.setItem("cb_reveal", JSON.stringify(response));
-    if (response && response.company && response.type === 'company' && response.company.domain) {
-        var company = { website: response.company.domain, source: 'clearbit' }
+  sessionStorage.setItem("cb_reveal", JSON.stringify(response));
 
-        if (response.company.name)
-            company.name = response.company.name;
-        if (response.company.geo.country)
-            company.country = response.company.geo.country;
-        if (response.company.category.industryGroup)
-            company.industry = response.company.category.industryGroup;
-        if (response.company.metrics.employeesRange)
-            company.number_of_employees = response.company.metrics.employeesRange;
-        if (response.company.metrics.estimatedAnnualRevenue)
-            company.annual_revenue = response.company.metrics.estimatedAnnualRevenue;
+  if (
+    !response ||
+    !response.company ||
+    response.type !== "company" ||
+    !response.company.domain
+  ) {
+    return;
+  }
 
-        analytics.group(null, company);
-    }
+  analytics.group(null, {
+    website: response.company.domain,
+    source: "clearbit",
+    name: response.company.name || undefined,
+    country: response.company.geo.country || undefined,
+    industry: response.company.category.industryGroup || undefined,
+    number_of_employees: response.company.metrics.employeesRange || undefined,
+    annual_revenue:
+      response.company.metrics.estimatedAnnualRevenue || undefined,
+  });
 }
 
 window.analytics.ready(function () {
-    const clearbit_key = '{pk_replace_me}'; 
-    var response = sessionStorage.getItem("cb_reveal");
-    if (!response && !analytics.group().traits().website) {
-        let s = document.createElement("script");
-        s.src = `https://reveal.clearbit.com/v1/companies/reveal?authorization=${clearbit_key}&callback=clearbitCallback`;
-        document.body.appendChild(s);
-    }
+  const clearbitKey = "{pk_replace_me}";
+  const response = sessionStorage.getItem("cb_reveal");
+
+  if (response || analytics.group().traits().website) {
+    return;
+  }
+
+  const script = document.createElement("script");
+  script.src = `https://reveal.clearbit.com/v1/companies/reveal?authorization=${clearbitKey}&callback=clearbitCallback`;
+  document.body.appendChild(script);
 });
