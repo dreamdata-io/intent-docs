eafaa79491689745fb1bb8a2e9af0d27bfd81d50
Ole Dallerup <dallerup@gmail.com>
2023-08-22 12:07:38 +0200

    updated clearbit docs

--- clearbit.js
+++ clearbit.js
@@ -1,36 +1,16 @@
-function clearbitCallback(response) {
-  sessionStorage.setItem("cb_reveal", JSON.stringify(response));
-
-  if (
-    !response ||
-    !response.company ||
-    response.type !== "company" ||
-    !response.company.domain
-  ) {
-    return;
-  }
-
-  analytics.group(null, {
-    website: response.company.domain,
-    source: "clearbit",
-    name: response.company.name || undefined,
-    country: response.company.geo.country || undefined,
-    industry: response.company.category.industryGroup || undefined,
-    number_of_employees: response.company.metrics.employeesRange || undefined,
-    annual_revenue:
-      response.company.metrics.estimatedAnnualRevenue || undefined,
-  });
-}
-
 window.analytics.ready(function () {
-  var clearbitKey = "{pk_replace_me}";
-  var response = sessionStorage.getItem("cb_reveal");
-
-  if (response || analytics.group().traits().website) {
+  if (!window.reveal || analytics.group().traits().website || window.reveal.type !== 'company') {
     return;
   }
 
-  var script = document.createElement("script");
-  script.src = `https://reveal.clearbit.com/v1/companies/reveal?authorization=${clearbitKey}&callback=clearbitCallback`;
-  document.body.appendChild(script);
+  var reveal = window.reveal;
+  analytics.group(null, {
+    website: reveal.company.domain,
+    source: "clearbit",
+    name: reveal.company.name || undefined,
+    country: reveal.company.geo.country || undefined,
+    industry: reveal.company.category.industryGroup || undefined,
+    number_of_employees: reveal.company.metrics.employeesRange || undefined,
+    annual_revenue: reveal.company.metrics.estimatedAnnualRevenue || undefined,
+  });
 });
